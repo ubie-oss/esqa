b6e444246076178281fd50ae769ba23abf4a69e0
Takahiko Ito <takahiko.ito@dr-ubie.com>
2021-12-31 21:58:37 +0900

    Support sub command

--- esqa/cli.py
+++ esqa/cli.py
@@ -7,12 +7,16 @@ from esqa.config import Configuration, load
 from esqa.runner import Runner
 
 
-@click.command()
+@click.group()
+def main():
+    pass
+
+@main.command()
 @click.option('-c', '--config', type=str, help="configuration file")
 @click.option('--es-host', type=str, help="Host name of Elasticsearch host", required=False)
 @click.option('--es-port', type=int, help="Port number of Elasticsearch host", required=False)
 @click.option('--index', type=str, help="target index name", required=True)
-def main(config, es_port, es_host, index):
+def check(config, es_port, es_host, index):
     runner = Runner()
     results = runner.run(config=load(config), index_name=index)
     print(results)
