402c7b6b35dfbc178793c3ef10e1656902601413
Takahiko Ito <takahiko.ito@dr-ubie.com>
2022-03-14 10:03:30 +0900

    Change validation setting format

--- esqa/validation_config.py
+++ esqa/validation_config.py
@@ -1,7 +1,7 @@
 import json
 import dataclasses
 import string
-from typing import List, Union
+from typing import List, Union, Dict
 
 
 @dataclasses.dataclass
@@ -44,28 +44,36 @@ def _generate_asserts(assert_config: list) -> List[EsAssert]:
     ]
 
 
-def _load_template_query(template_query: dict) -> dict:
-    template_file_path = template_query["template"]
-    with open(template_file_path) as t:
-        template = string.Template(t.read())
-    return json.loads(template.substitute(**template_query))
+def _load_template_query(template_query: dict, templates: Dict[str, string.Template]) -> dict:
+    template_name = template_query["template"]
+    return json.loads(templates[template_name].substitute(**template_query))
 
 
-def _load_query(query: dict) -> dict:
+def _load_query(query: dict, templates: Dict[str, string.Template]) -> dict:
     if "template" in query:
-        return _load_template_query(query)
+        return _load_template_query(query, templates)
     return query
 
 
-def _generate_cases(cases: List[dict]) -> List[Case]:
+def _generate_cases(cases: List[dict], templates: Dict[str, string.Template]) -> List[Case]:
     return [
         Case(
             element["name"],
-            _load_query(element["query"]),
+            _load_query(element["query"], templates),
             _generate_asserts(element["asserts"]),
         )
         for element in cases
     ]
+
+
+def _load_templates(template_settings: dict) -> Dict[str, string.Template]:
+    templates: Dict[str, string.Template] = {}
+    for setting in template_settings:
+        template_file_path = setting["path"]
+        with open(template_file_path) as t:
+            template = string.Template(t.read())
+        templates[setting["name"]] = template
+    return templates
 
 
 def generate(config: dict) -> Configuration:
@@ -74,7 +82,11 @@ def generate(config: dict) -> Configuration:
     :param config: dictionary object containing configuration settings
     :return: Configuration object
     """
-    return Configuration(_generate_cases(config["cases"]))
+    if "templates" in config:
+        templates = _load_templates(config["templates"])
+    else:
+        templates = []
+    return Configuration(_generate_cases(config["cases"], templates))
 
 
 def load(file_path: str) -> Configuration:
