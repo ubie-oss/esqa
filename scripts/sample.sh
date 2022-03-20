b60fc4d17d9b282a78689c4ead0c8854d9d30ada
Takahiko Ito <takahiko.ito@dr-ubie.com>
2022-03-20 23:21:18 +0900

    Fix samples

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -2,5 +2,7 @@
 
 set -euo pipefail -o posix
 
+export ELASTICSEARCH_URL=localhost:9400
+
 esqa save --config sample/ranking.json --index sample > ~/Downloads/ranking.json
 esqa ranking --config sample/ranking.json --index sample --ranking ~/Downloads/ranking.json
