7bb20bad1d3d8092dfabac7b3137abd17f44bf0e
Takahiko Ito <takahiko.ito@dr-ubie.com>
2022-03-20 23:34:28 +0900

    Make sample script flush result to output

--- scripts/sample.sh
+++ scripts/sample.sh
@@ -4,5 +4,9 @@ set -euo pipefail -o posix
 
 export ELASTICSEARCH_URL=localhost:9400
 
-esqa save --config sample/ranking.json --index sample > ~/Downloads/ranking.json
-esqa ranking --config sample/ranking.json --index sample --ranking ~/Downloads/ranking.json
+if [ ! -d output ]; then
+  mkdir output
+fi
+
+esqa save --config sample/ranking.json --index sample > output/ranking.json
+esqa ranking --config sample/ranking.json --index sample --ranking output/ranking.json
